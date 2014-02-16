55dc17a3ba661832774498ef185d3c4f2424e3f4
Kenny Tay <ktay522@gmail.com>
2014-02-16 15:30:33 -0800

    Make post title and content editable

--- views.py
+++ views.py
@@ -24,7 +24,6 @@ Bugs/pending issues:
 22. Upvote/downvote buttons could be fancier
 23. Delete in profile page should redirect to profile page
 26. Upload and annotate pages should be combined
-27. Edit post needs to be editable
 28. Next
 '''
 
